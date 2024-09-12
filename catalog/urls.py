88dc20838e457de516721d8305c0ac6c1f607666
aptik <aptik@DESKTOP-SVN8V0A>
2024-09-12 19:08:03 +0300

    обавлены шаблоны и контроллеры для главной страницы и контактов

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('', include('catalog.urls')),  # Подключаем маршруты приложения catalog
+]
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
 ]
