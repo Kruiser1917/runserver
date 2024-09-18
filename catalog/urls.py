706e91e50688acf157f04734a1defb9e8afe1f2f
aptik <aptik@DESKTOP-SVN8V0A>
2024-09-18 12:17:55 +0300

    Создан новый проект, добавлено приложение catalog и исправлены настройки

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,14 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('catalog.urls')),  # Подключаем маршруты приложения catalog
-]
-
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
+    path('', views.home, name='home'),  # Главная страница
+    path('contacts/', views.contacts, name='contacts'),  # Страница контактов
 ]
